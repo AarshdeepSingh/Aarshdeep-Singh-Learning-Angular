16afcc3538afb84456abf3063a58d34580f6e170
Aarshdeep Singh <w0838091@myscc.ca>
2024-09-18 14:35:27 -0400

    #3 add the following
    1. initialize an array with 6 instances of the project.
    
    2. Create html template to show all the instances
    
    3. Create basic css for the html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {product} from "./project.interface";
+import {NgForOf, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgForOf, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -12,4 +14,14 @@ export class AppComponent {
   title = 'Aarshdeep-Singh-Learning-Angular';
   name : String = 'Aarshdeep Singh';
   age : number = 20;
+
+  products: product[] = [
+    {id: 1, name: 'Starter pack', description: 'contain starter pokemon cards', price: 10, image: '',rarity: 'uncommon'},//no image for now
+    {id: 2, name: 'Basic pack', description: 'contain Basic pokemon cards', price: 5, image: '',rarity:'common'},
+    {id: 3, name: 'Legendary pack', description: 'may contain legendary pokemon cards', price: 25, image: '',rarity: 'ultra rare'},
+    {id: 4, name: 'Mythical pack', description: 'may contain mythical pokemon cards', price: 20, image: '',rarity: 'rare'},
+    {id: 5, name: 'gen-1 pack', description: 'contain only gen-1 pokemon cards', price: 12, image: ''},
+    {id: 6, name: 'gen-2 pack', description: 'contain only gen-2 pokemon cards', price: 12, image: ''}
+  ]
+
 }
