b75d7aa59f182a56f8512604543132228480b1f6
Aarshdeep Singh <w0838091@myscc.ca>
2024-10-12 19:41:30 -0400

    # 16 create a list of routes

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,22 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import {provideRouter} from "@angular/router";
+import { HomeComponent } from './app/home/home.component';
+import { ModifyListItemsComponent } from './app/modify-list-items/modify-list-items.component';
+import { PageNotFoundComponent } from './app/page-not-found/page-not-found.component'; //
+import {PokemonCardsListComponent} from "./app/pokemon-cards-list/pokemon-cards-list.component";
 
-bootstrapApplication(AppComponent, appConfig)
+
+const routes = [
+  {path: '', component: HomeComponent},
+  {path: 'modify', component: ModifyListItemsComponent},
+  {path: '**', component: PageNotFoundComponent}
+];
+
+bootstrapApplication(AppComponent, {
+  providers: [
+  provideRouter(routes)
+]
+})
   .catch((err) => console.error(err));
