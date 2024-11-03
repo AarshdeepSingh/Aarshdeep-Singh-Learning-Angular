a26a0fb0cbfde05b10b81a1d35167bc3de3f3d75
Aarshdeep Singh <w0838091@myscc.ca>
2024-11-02 20:49:38 -0400

    # 20  add methods to handle editing and deleting items

--- src/app/pokemon-cards-list/pokemon-cards-list.component.ts
+++ src/app/pokemon-cards-list/pokemon-cards-list.component.ts
@@ -1,20 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router'; // Import Router for navigation
-import { PokemonCardsListItemComponent } from "../pokemon-cards-list-item/pokemon-cards-list-item.component";
-import { NgClass, NgForOf, NgStyle } from "@angular/common";
-import { PokemonCardsService } from "../service/Pokemon-Cards-service.service";
-import { PokemonCards } from "../project.interface";
+import { Router } from '@angular/router';
+import { PokemonCardsService } from '../service/Pokemon-Cards-service.service';
+import { PokemonCards } from '../project.interface';
 
 @Component({
   selector: 'app-pokemon-cards-list',
-  standalone: true,
-  imports: [
-    PokemonCardsListItemComponent,
-    NgStyle,
-    NgClass,
-    NgForOf
-  ],
   templateUrl: './pokemon-cards-list.component.html',
+  standalone: true,
   styleUrls: ['./pokemon-cards-list.component.css']
 })
 export class PokemonCardsListComponent implements OnInit {
@@ -30,8 +22,7 @@ export class PokemonCardsListComponent implements OnInit {
   loadPokemonCards() {
     this.pokemonCardsService.getPokemonCards().subscribe({
       next: (data: PokemonCards[]) => this.PokemonCardsList = data,
-      error: (err: any) => console.error("Error fetching Pokemon cards", err),
-      complete: () => console.log("Pokemon cards data fetch complete")
+      error: (err: any) => console.error("Error fetching Pokemon cards", err)
     });
   }
 
@@ -41,7 +32,7 @@ export class PokemonCardsListComponent implements OnInit {
 
   deleteItem(id: number) {
     this.pokemonCardsService.deleteGame(id).subscribe(() => {
-      this.loadPokemonCards(); // Reload the list after deletion
+      this.loadPokemonCards();
     });
   }
 
--- src/app/service/Pokemon-Cards-service.service.ts
+++ src/app/service/Pokemon-Cards-service.service.ts
@@ -20,20 +20,21 @@ export class PokemonCardsService {
     return of(foundCard);
   }
 
-  addPokemonCard(newCard: PokemonCards): Observable<PokemonCards> {
+  addPokemonCard(newCard: PokemonCards): Observable<PokemonCards[]> {
     newCard.id = this.PokemonCards.length ? Math.max(...this.PokemonCards.map(card => card.id)) + 1 : 1;
     this.PokemonCards.push(newCard);
-    return of(newCard);
+    return of(this.PokemonCards); // Return the updated list
   }
 
-  updatePokemonCard(updatedCard: PokemonCards): Observable<PokemonCards | undefined> {
+  updatePokemonCard(updatedCard: PokemonCards): Observable<PokemonCards[] | undefined> {
     const index = this.PokemonCards.findIndex(card => card.id === updatedCard.id);
     if (index !== -1) {
       this.PokemonCards[index] = updatedCard;
-      return of(updatedCard);
+      return of(this.PokemonCards); // Return the updated list
     }
     return of(undefined);
   }
+
 
   deleteGame(id: number): Observable<PokemonCards[]> {
     this.PokemonCards = this.PokemonCards.filter(item => item.id !== id);
