2846f864591c749a90be91577ad77a00b3838c22
Aarshdeep Singh <w0838091@myscc.ca>
2024-11-02 20:43:24 -0400

    # 19 Complete the following
    1.  add buttons to edit and delete items.
    2.  create a form with input fields and buttons  for adding or updating items.

--- src/app/pokemon-cards-list/pokemon-cards-list.component.ts
+++ src/app/pokemon-cards-list/pokemon-cards-list.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'; // Import Router for navigation
 import { PokemonCardsListItemComponent } from "../pokemon-cards-list-item/pokemon-cards-list-item.component";
 import { NgClass, NgForOf, NgStyle } from "@angular/common";
-import { PokemonCardsService } from "../service/Pokemon-Cards-service.service"; // Import the service
-import { PokemonCards } from "../project.interface"; // Ensure this is imported correctly
+import { PokemonCardsService } from "../service/Pokemon-Cards-service.service";
+import { PokemonCards } from "../project.interface";
 
 @Component({
   selector: 'app-pokemon-cards-list',
@@ -14,19 +15,37 @@ import { PokemonCards } from "../project.interface"; // Ensure this is imported 
     NgForOf
   ],
   templateUrl: './pokemon-cards-list.component.html',
-  styleUrls: ['./pokemon-cards-list.component.css'] // Corrected to plural
+  styleUrls: ['./pokemon-cards-list.component.css']
 })
 export class PokemonCardsListComponent implements OnInit {
 
   PokemonCardsList: PokemonCards[] = [];
 
-  constructor(private pokemonCardsService: PokemonCardsService) {} // Use the correct type here
+  constructor(private pokemonCardsService: PokemonCardsService, private router: Router) {}
 
   ngOnInit(): void {
+    this.loadPokemonCards();
+  }
+
+  loadPokemonCards() {
     this.pokemonCardsService.getPokemonCards().subscribe({
       next: (data: PokemonCards[]) => this.PokemonCardsList = data,
       error: (err: any) => console.error("Error fetching Pokemon cards", err),
       complete: () => console.log("Pokemon cards data fetch complete")
     });
+  }
+
+  editItem(card: PokemonCards) {
+    this.router.navigate(['/modify'], { state: { item: card } });
+  }
+
+  deleteItem(id: number) {
+    this.pokemonCardsService.deleteGame(id).subscribe(() => {
+      this.loadPokemonCards(); // Reload the list after deletion
+    });
+  }
+
+  goToAddItem() {
+    this.router.navigate(['/modify']);
   }
 }
