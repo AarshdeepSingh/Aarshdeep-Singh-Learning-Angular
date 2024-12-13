2703ebfae1c1446daa89231df8fd9cd0570f525a
Aarshdeep Singh <w0838091@myscc.ca>
2024-12-13 00:23:57 -0500

    # 28 assignment 10

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import {provideRouter} from "@angular/router";
+import {provideRouter, Routes} from "@angular/router";
 import { HomeComponent } from './app/home/home.component';
 import { ModifyListItemsComponent } from './app/modify-list-items/modify-list-items.component';
 import { PageNotFoundComponent } from './app/page-not-found/page-not-found.component'; //
 import {PokemonCardsListComponent} from "./app/pokemon-cards-list/pokemon-cards-list.component";
 
 
-const routes = [
+const routes: Routes  = [
   {path: '', component: HomeComponent},
   {path: 'modify', component: ModifyListItemsComponent},
   {path: '**', component: PageNotFoundComponent}
